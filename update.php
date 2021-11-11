585901ecdd0dcba8294bce0ce92776cdc338853a
Juan Camilo Torres <juancamilotorres@ucundinamarca.edu.co>
2021-11-11 11:13:32 -0500

    delete y update

--- update.php
+++ update.php
@@ -3,8 +3,8 @@
 include("conexion.php");
 $con=conectar();
 
-$id=$_POST['cod_estudiante'];
-$cedula=$_POST['dni'];
+$id=$_POST['id'];
+$cedula=$_POST['cedula'];
 $nombre=$_POST['nombre'];
 $apellidos=$_POST['apellidos'];
 
@@ -12,6 +12,6 @@ $sql="UPDATE administrador SET  cedula='$cedula',nombre='$nombre',apellidos='$ap
 $query=mysqli_query($con,$sql);
 
     if($query){
-        Header("Location: alumno.php");
+        Header("Location: administradorFuncionarios.html");
     }
 ?>
