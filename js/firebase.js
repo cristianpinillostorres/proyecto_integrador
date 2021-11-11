1c7ed76e0ef4716d9fa740ca77228e00af4256ab
cristianpinillostorres <cristianpinillos@gmail.com>
2021-11-10 22:56:33 -0500

    commit

---
+++ js/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9AleI83nTxoFGQ2Om3SzfOoxfT9Y9STA",
+  authDomain: "proyectointegrador-65b53.firebaseapp.com",
+  projectId: "proyectointegrador-65b53",
+  storageBucket: "proyectointegrador-65b53.appspot.com",
+  messagingSenderId: "229701552046",
+  appId: "1:229701552046:web:4ae0278beea1ca03392a3f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+window.salir = function salir() {
+      const auth = getAuth();
+      signOut(auth).then(() =>{
+            document.location.href = "/index.html";
+      }).catch((err) =>{
+          alert("Se produce error al cerrar la sesion");
+          console.log(err);
+      });
+}
---
+++ js/index.js
@@ -0,0 +1,60 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, TwitterAuthProvider, GithubAuthProvider,FacebookAuthProvider} from "https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js";
+
+
+window.iniciarSesion = function iniciarSesion() {
+
+    const email = document.getElementById("txtcorreo").value;
+    const password = document.getElementById("txtcontrasenia").value;
+
+    if (email == "" || password == "") {
+        document.getElementById("alertErrorLogueo").style.display = "block";
+        document.getElementById("alertErrorLogueo").innerHTML = "Email y/o contraseña son obligatorios";
+        return false;
+
+    } else {
+
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+            //va al index
+            document.location.href = "/index-register.html";
+        }).catch((error) => {
+            document.getElementById("alertErrorLogueo").style.display = "block";
+            document.getElementById("alertErrorLogueo").innerHTML = error.message;
+        });
+    }
+
+}
+
+window.authGoogle = function authGoogle() {
+    const provider = new GoogleAuthProvider();
+    authGeneric(provider, "Google");
+}
+
+window.authTwitter = function authTwitter() {
+    const provider = new TwitterAuthProvider();
+    authGeneric(provider, "Twitter");
+}
+
+window.authGithub = function authGithub() {
+    const provider = new GithubAuthProvider();
+    authGeneric(provider, "GitHub");
+}
+
+window.authFacebook = function authFacebook() {
+    const provider = new FacebookAuthProvider();
+    authGeneric(provider, "Facebook");
+}
+
+function authGeneric(provider, providerName) {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            actualizarPerfil(result.user, providerName);
+        }).catch((error) => {
+            //const errorCode = error.code;
+            //const email = error.email;
+            const errorMessage = error.message;
+            document.getElementById("alertErrorLogueo").style.display = "block";
+            document.getElementById("alertErrorLogueo").innerHTML = errorMessage;
+        });
+}
