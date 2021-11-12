dc0460e3e0ea86c136b79446f383e80c805e99e6
cristianpinillostorres <cristianpinillos@gmail.com>
2021-11-12 13:06:03 -0500

    se agrega crud clientes

--- js/administradorClientes.js
+++ js/administradorClientes.js
@@ -4,11 +4,11 @@ import { verAutenticacion } from "./firebase.js";
 
 const db = getFirestore();
 var operacion = 1;
-var idUsuarioGlobal;
+var idClienteGlobal;
 
 window.onload = function () {
     verAutenticacion();
-    cargarUsuarios();
+    cargarClientes();
 }
 
 window.operar = function operar() {
@@ -20,58 +20,46 @@ window.operar = function operar() {
     }
 }
 
-window.abrirModal = function abrirModal(idUsuario) {
-    if (idUsuario == 0) {
+window.abrirModal = function abrirModal(idCliente) {
+    if (idCliente == 0) {
         operacion = 1;
     } else {
         operacion = 2;
-        idUsuarioGlobal = idUsuario;
-        cargarDatos(idUsuario);
-
-        //guardar(band,idUsuario);
+        idClienteoGlobal = idCliente;
+        cargarDatos(idCliente);
     }
     limpiarDatos();
 }
 
 function limpiarDatos() {
-    document.getElementById("txtUsuario").value = "";
-    document.getElementById("txtCorreo").value = "";
     document.getElementById("txtNombre").value = "";
-    document.getElementById("txtApellido").value = "";
-    document.getElementById("txtCedula").value = "";
+    document.getElementById("txtNit").value = "";
     document.getElementById("txtTelefono").value = "";
-    document.getElementById("txtRol").value = "";
-
     document.getElementById("alertaErrorCrearUsuario").style.display = "none";
     document.getElementById("alertaErrorCrearUsuario").innerHTML = "";
 }
 
-function cargarUsuarios() {
+function cargarClientes() {
 
     let contenido = "<table class='table mt-2'>";
 
     contenido += "<thead>";
     contenido += "<tr>";
-    contenido += "<th>Nombre</th>";
-    contenido += "<th>Apellido</th>";
+    contenido += "<th>Nombre Compañia</th>";
+    contenido += "<th>Nit</th>";
     contenido += "<th>Telefono</th>";
-    contenido += "<th>Rol</th>";
-    contenido += "<th>Opciones</th>";
-
     contenido += "</tr>";
     contenido += "</thead>";
-
     contenido += "<tbody>";
 
-    const q = query(collection(db, "usuarios"), where("visible", "==", true));
+    const q = query(collection(db, "clientes"), where("visible", "==", true));
     getDocs(q).then(querySnapshot => {
         querySnapshot.forEach(rpta => {
             const fila = rpta.data();
             contenido += "<tr>";
             contenido += "<td>" + fila.nombre + "</td>";
-            contenido += "<td>" + fila.apellido + "</td>";
+            contenido += "<td>" + fila.nit + "</td>";
             contenido += "<td>" + fila.telefono + "</td>";
-            contenido += "<td>" + fila.rol + "</td>";
             contenido += "<td>";
             contenido += "<input type='button' class='btn btn-info' value='Editar' onclick='abrirModal(\"" + rpta.id + "\")' data-toggle='modal' data-target='#exampleModal' />";
             contenido += "&nbsp&nbsp&nbsp&nbsp&nbsp";
@@ -79,10 +67,9 @@ function cargarUsuarios() {
             contenido += "</td>";
             contenido += "</tr>";
         });
-
         contenido += "</tbody>";
         contenido += "</table>";
-        document.getElementById("divUsuarios").innerHTML = contenido;
+        document.getElementById("divClientes").innerHTML = contenido;
 
     }).catch((error) => {
         console.log(error);
@@ -90,101 +77,74 @@ function cargarUsuarios() {
 }
 
 function guardar() {
-
     //Agregar validaciones
-    const usuario = document.getElementById("txtUsuario").value;
-    const correo = document.getElementById("txtCorreo").value;
     const nombre = document.getElementById("txtNombre").value;
-    const apellido = document.getElementById("txtApellido").value;
-    const cedula = document.getElementById("txtCedula").value;
+    const nit = document.getElementById("txtNit").value;
     const telefono = document.getElementById("txtTelefono").value;
-    const rol = document.getElementById("txtRol").value
 
+    const newCliente= doc(collection(db, "clientes"));
+    setDoc(newCliente, {
 
-    const newUser = doc(collection(db, "usuarios"));
-    setDoc(newUser, {
-        usuario: usuario,
-        correo: correo,
         nombre: nombre,
-        apellido: apellido,
-        cedula: cedula,
+        nit: nit,
         telefono: telefono,
-        rol: rol,
         visible: true
     }).then(() => {
         alert("Agregado correctamente");
         location.reload();
 
     }).catch(error => {
-        document.getElementById("alertaErrorCrearUsuario").style.display = "block";
-        document.getElementById("alertaErrorCrearUsuario").innerHTML = error.errorMessage;
+        document.getElementById("alertaErrorCrearCliente").style.display = "block";
+        document.getElementById("alertaErrorCrearCliente").innerHTML = error.errorMessage;
     });
 }
 
 function editar() {
      //Agregar validaciones
-     const usuario = document.getElementById("txtUsuario").value;
-     const correo = document.getElementById("txtCorreo").value;
      const nombre = document.getElementById("txtNombre").value;
-     const apellido = document.getElementById("txtApellido").value;
-     const cedula = document.getElementById("txtCedula").value;
+     const nit = document.getElementById("txtNit").value;
      const telefono = document.getElementById("txtTelefono").value;
-     const rol = document.getElementById("txtRol").value
 
-    const usuarioRef = doc(db, "usuarios", idUsuarioGlobal);
-    updateDoc(usuarioRef, {
-        usuario: usuario,
-        correo: correo,
+    const clienteRef = doc(db, "clientes", idClienteGlobal);
+    updateDoc(clienteRef, {
         nombre: nombre,
-        apellido: apellido,
-        cedula: cedula,
+        nit: nit,
         telefono: telefono,
-        rol: rol,
         visible: true
     }).then(() => {
         alert("Editado correctamente");
         cargarUsuarios();
     }).catch(error => {
-        document.getElementById("alertaErrorCrearUsuario").style.display = "block";
-        document.getElementById("alertaErrorCrearUsuario").innerHTML = error.errorMessage;
-
+        document.getElementById("alertaErrorCrearCliente").style.display = "block";
+        document.getElementById("alertaErrorCrearCliente").innerHTML = error.errorMessage;
     });
 }
 
-window.eliminar = function eliminar(idUser) {
-    const usuarioRef = doc(db, "usuarios", idUser);
-    updateDoc(usuarioRef, {
+window.eliminar = function eliminar(idClientes) {
+    const clienteRef = doc(db, "clientes", idClientes);
+    updateDoc(clienteRef, {
         visible: false
     }).then(() => {
         alert("Eliminado correctamente");
-        cargarUsuarios();
+        cargarClientes();
     }).catch((error) => {
         alert("Ocurrio un error al eliminar");
         console.log(error);
     });
 }
 
-function cargarDatos(idUsuario) {
-
-    const docRef = doc(db, "usuarios", idUsuario);
+function cargarDatos(idClientes) {
+    const docRef = doc(db, "clientes", idClientes);
     getDoc(docRef).then(docSnap => {
         if (docSnap.exists()) {
-
             const data = docSnap.data();
-
-            document.getElementById("txtUsuario").value = data.usuario;
-            document.getElementById("txtCorreo").value = data.correo;
             document.getElementById("txtNombre").value = data.nombre;
-            document.getElementById("txtApellido").value = data.apellido
-            document.getElementById("txtCedula").value = data.cedula;
+            document.getElementById("txtNit").value = data.nit;
             document.getElementById("txtTelefono").value = data.telefono;
-            document.getElementById("txtRol").value = data.rol;
-
         } else {
             alert("No se puede encontrar el usuario");
         }
     }).catch((error) => {
         alert("Ocurrio un error" + error.message);
     });
-
 }
